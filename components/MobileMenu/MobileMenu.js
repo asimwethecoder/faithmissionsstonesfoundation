0ef0397ef76e5fa5e502fc8472da0be7ce1460ba
asimwethecoder <143520728+asimwethecoder@users.noreply.github.com>
2025-06-01 21:11:57 +0200

    treamed pages

--- components/MobileMenu/MobileMenu.js
+++ components/MobileMenu/MobileMenu.js
@@ -1,92 +1,87 @@
-import React, { Fragment, useState } from "react";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/List";
-import Collapse from "@mui/material/Collapse";
-import Link from "next/link";
+import React, { Fragment, useState } from 'react';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/List';
+import Collapse from '@mui/material/Collapse';
+import Link from 'next/link';
 
 const menus = [
   {
     id: 1,
-    title: "Home",
-    link: "/home-2",
+    title: 'Home',
+    link: '/home-2',
   },
   {
     id: 2,
-    title: "Pages",
-    link: "#",
+    title: 'Pages',
+    link: '#',
     submenu: [
       {
         id: 21,
-        title: "About",
-        link: "/about",
+        title: 'About',
+        link: '/about',
       },
       {
         id: 22,
-        title: "Donate",
-        link: "/donate",
+        title: 'Donate',
+        link: '/donate',
       },
       {
         id: 25,
-        title: "Causes",
-        link: "/causes",
+        title: 'Causes',
+        link: '/causes',
       },
       {
         id: 26,
-        title: "Causes Single",
-        link: "/causes-single/established-fact",
+        title: 'Causes Single',
+        link: '/causes-single/established-fact',
       },
       {
         id: 27,
-        title: "Volunteer",
-        link: "/volunteer-1",
-      },
-      {
-        id: 203,
-        title: "404 Error",
-        link: "/404",
+        title: 'Volunteer',
+        link: '/volunteer-1',
       },
     ],
   },
 
   {
     id: 4,
-    title: "Services",
-    link: "#",
+    title: 'Services',
+    link: '#',
     submenu: [
       {
         id: 41,
-        title: "Services",
-        link: "/service",
+        title: 'Services',
+        link: '/service',
       },
       {
         id: 42,
-        title: "Service Single",
-        link: "/service-single/Fund-Raised-&-Donation",
+        title: 'Service Single',
+        link: '/service-single/Fund-Raised-&-Donation',
       },
     ],
   },
   {
     id: 5,
-    title: "Portfolio",
-    link: "#",
+    title: 'Portfolio',
+    link: '#',
     submenu: [
       {
         id: 51,
-        title: "Portfolio",
-        link: "/project",
+        title: 'Portfolio',
+        link: '/project',
       },
       {
         id: 52,
-        title: "Portfolio Single",
-        link: "/project-single/Custom-Fabrication",
+        title: 'Portfolio Single',
+        link: '/project-single/Custom-Fabrication',
       },
     ],
   },
 
   {
     id: 88,
-    title: "Contact",
-    link: "/contact",
+    title: 'Contact',
+    link: '/contact',
   },
 ];
 
@@ -100,51 +95,46 @@ const MobileMenu = () => {
 
   return (
     <div>
-      <div className={`mobileMenu ${menuActive ? "show" : ""}`}>
-        <div className="menu-close">
-          <div className="clox" onClick={() => setMenuState(!menuActive)}>
-            <i className="ti-close"></i>
+      <div className={`mobileMenu ${menuActive ? 'show' : ''}`}>
+        <div className='menu-close'>
+          <div className='clox' onClick={() => setMenuState(!menuActive)}>
+            <i className='ti-close'></i>
           </div>
         </div>
 
-        <ul className="responsivemenu">
+        <ul className='responsivemenu'>
           {menus.map((item, mn) => {
             return (
               <ListItem
-                className={item.id === openId ? "active" : null}
-                key={mn}
-              >
+                className={item.id === openId ? 'active' : null}
+                key={mn}>
                 {item.submenu ? (
                   <Fragment>
                     <p
                       onClick={() =>
                         setOpenId(item.id === openId ? 0 : item.id)
-                      }
-                    >
+                      }>
                       {item.title}
                       <i
                         className={
                           item.id === openId
-                            ? "fa fa-angle-up"
-                            : "fa fa-angle-down"
-                        }
-                      ></i>
+                            ? 'fa fa-angle-up'
+                            : 'fa fa-angle-down'
+                        }></i>
                     </p>
                     <Collapse
                       in={item.id === openId}
-                      timeout="auto"
-                      unmountOnExit
-                    >
-                      <List className="subMenu">
+                      timeout='auto'
+                      unmountOnExit>
+                      <List className='subMenu'>
                         <Fragment>
                           {item.submenu.map((submenu, i) => {
                             return (
                               <ListItem key={i}>
                                 <Link
                                   onClick={ClickHandler}
-                                  className="active"
-                                  href={submenu.link}
-                                >
+                                  className='active'
+                                  href={submenu.link}>
                                   {submenu.title}
                                 </Link>
                               </ListItem>
@@ -155,7 +145,7 @@ const MobileMenu = () => {
                     </Collapse>
                   </Fragment>
                 ) : (
-                  <Link className="active" href={item.link}>
+                  <Link className='active' href={item.link}>
                     {item.title}
                   </Link>
                 )}
@@ -166,13 +156,12 @@ const MobileMenu = () => {
       </div>
 
       <div
-        className="showmenu mobail-menu"
-        onClick={() => setMenuState(!menuActive)}
-      >
-        <button type="button" className="navbar-toggler open-btn">
-          <span className="icon-bar first-angle"></span>
-          <span className="icon-bar middle-angle"></span>
-          <span className="icon-bar last-angle"></span>
+        className='showmenu mobail-menu'
+        onClick={() => setMenuState(!menuActive)}>
+        <button type='button' className='navbar-toggler open-btn'>
+          <span className='icon-bar first-angle'></span>
+          <span className='icon-bar middle-angle'></span>
+          <span className='icon-bar last-angle'></span>
         </button>
       </div>
     </div>
